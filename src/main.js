eca5a94f3b2466bf9db26e6b065fb89ed72eae7d
NKDuy <nkduy.dev@gmail.com>
2024-01-15 21:02:58 +0700

    chore: add router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'kdu'
-import './style.css'
-import App from './App.kdu'
+import { createApp } from "kdu";
+import "./style.css";
+import App from "./App.kdu";
+import router from "./router";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
